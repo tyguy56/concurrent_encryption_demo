6a4b11631d251e5a76a9092e9b28367e68bb152f
Tyler Lund <tyguy56@gmail.com>
2022-05-01 19:08:18 -0500

    cleaned code and added additional comments TODO: fix buffer undersize error

--- encrypt-drive.c
+++ encrypt-drive.c
@@ -1,3 +1,32 @@
+/**
+ * @file encrypt-drive.c
+ * @author Tyler Lund (tblund@iastate.edu)
+ * @brief
+ * Simple concurrent program responsible for taking a file in encrypting it, counting the characters before and
+ * after encryption and then writting the output to a specificed output file. The code works by using a series of
+ * pthreads and semaphores to lock individual processes. Initally the code requests the user for a specificed buffer
+ * size which is used to create the input and output buffers which are in turn used to hold the characters as they are
+ * being processesed by the varius threads. The code then initalizes several semaphores specified as follows
+ *
+ *      inMutex:        used to block threads from consuming the character in the input buffer the processes is working on
+ *      outMutex:       used to block threads from consuming the character in the output buffer the processes is working on
+ *      encryptFull:    used to block and signal the encryption thread that a new character has been placed in the input buffer
+ *      encryptEmpty:   used to block and signal the encryption thread that the output and writer threads have space for a new character
+ *      readEmpty:      used to block and signal the reader thread that the input buffer has space for a new character
+ *      writeFull:      used to block and signal the writer thread that the output buffer has a new character from the encryption thread
+ *      inFull:         used to block and signal the input counter thread that the input buffer has a new character from the reader thread (solves cold start issue)
+ *      outFull:        used to block and signal the output counter thread that the output buffer has a new character from the encryption thread
+ *
+ * The code proceeds to create and execute the required five pthreads to work in conjunction waiting for each thread to rejoin, log counts and then destroy
+ * the semaphores
+ *
+ * @version 0.1
+ * @date 2022-05-01
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -7,14 +36,6 @@
 
 char *in_buf;
 char *out_buf;
-/*
-sem_t read_sem;
-sem_t *process_sem;
-sem_t *out_process_sem;
-sem_t read_finished;
-sem_t encrypt_sem;
-sem_t encrypt_finished_sem;
-*/
 
 sem_t inMutex;
 sem_t outMutex;
@@ -23,23 +44,16 @@ sem_t encryptFull, encryptEmpty, readEmpty, writeFull, inFull, outFull;
 int buffer_size;
 int reader_index;
 int encrypt_index;
-// int read_done;
-pthread_cond_t buffer_not_full = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
-pthread_cond_t out_buffer_not_full = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t out_lock = PTHREAD_MUTEX_INITIALIZER;
 
 /* Thread function prototypes */
-void *runReaderThread(void *arg);        /* reader thread runner */
-void *runWriterThread(void *arg);        /* writer thread runner */
-void *runEncryptionThread(void *arg);    /* encryption thread runner */
-void *runInputCounterThread(void *arg);  /* input counter thread runner */
-void *runOutputCounterThread(void *arg); /* output counter thread runner */
+void *runReaderThread();        /* reader thread runner */
+void *runWriterThread();        /* writer thread runner */
+void *runEncryptionThread();    /* encryption thread runner */
+void *runInputCounterThread();  /* input counter thread runner */
+void *runOutputCounterThread(); /* output counter thread runner */
 
 int main(int argc, char *argv[])
 {
-    // read_done = 0;
     pthread_t reader, input_counter, encryption, output_counter, writer;
 
     // 1) check number of command arguments exiting if incorrect
@@ -74,23 +88,7 @@ int main(int argc, char *argv[])
     sem_init(&outFull, 0, 0);
     sem_init(&inMutex, 0, 1);
     sem_init(&outMutex, 0, 1);
-    /*
-        sem_init(&read_finished, 0, 0);
 
-        sem_init(&encrypt_finished_sem, 0, 0);
-
-        process_sem = malloc(sizeof(sem_t) * buffer_size);
-        for (int i = 0; i < buffer_size; i++)
-        {
-            sem_init(&process_sem[i], 0, 0);
-        }
-
-        out_process_sem = malloc(sizeof(sem_t) * buffer_size);
-        for (int i = 0; i < buffer_size; i++)
-        {
-            sem_init(&out_process_sem[i], 0, 0);
-        }
-    */
     // 5) create other threads
     printf("Creating READER thread...\n");
     pthread_create(&reader, NULL, runReaderThread, NULL); // start the reader thread in the calling process
@@ -110,15 +108,10 @@ int main(int argc, char *argv[])
     // 6) wait for all threads to complete
 
     pthread_join(reader, NULL);
-    printf("killing reader \n");
     pthread_join(input_counter, NULL);
-    printf("killing input_counter \n");
     pthread_join(encryption, NULL);
-    printf("killing encryption \n");
     pthread_join(output_counter, NULL);
-    printf("killing output_counter \n");
     pthread_join(writer, NULL);
-    printf("killing writer OUTPUT \n");
 
     // 7) log character counts
     log_counts();
@@ -137,69 +130,39 @@ int main(int argc, char *argv[])
 /**
  * runs the readerThread responsible for reading from the file and putting characters into the input buffer
  *
- * @param arg
  * @return void*
  */
 
-void *runReaderThread(void *arg)
+void *runReaderThread()
 {
-    (void)arg;
     char c;
     reader_index = 0;
-    int value;
 
     while ((c = read_input()) != EOF)
     {
-        // printf("locking read empty\n");
-        // fflush(stdout);
         sem_wait(&readEmpty);
-        // printf("locking in mutex\n");
-        // fflush(stdout);
         sem_wait(&inMutex);
         in_buf[reader_index] = c;
-        // printf("read:%c\n", in_buf[reader_index]);
-        // fflush(stdout);
         sem_post(&inMutex);
         sem_post(&encryptFull);
         sem_post(&inFull);
         reader_index = (reader_index + 1) % buffer_size;
     }
-    // printf("end of reader\n");
-    // fflush(stdout);
     sem_post(&inFull);
     sem_post(&encryptFull);
     reader_index = (reader_index + 1) % buffer_size;
     in_buf[reader_index] = EOF;
-    /*
-        pthread_mutex_lock(&lock);
-        while ((c = read_input()) != EOF)
-        {
-
-            sem_getvalue(&process_sem[reader_index], &value);
-            if (value != 0)
-            {
-                pthread_cond_wait(&buffer_not_full, &lock);
-            }
-            in_buf[reader_index] = c;
-            sem_post(&process_sem[reader_index]);
-            // sem_post(&process_sem[reader_index]);
-            reader_index = ((1 + reader_index) % buffer_size);
-        }
-        pthread_mutex_unlock(&lock);
-
-        sem_post(&read_finished);
-        // sem_post(&process_sem[reader_index]);
-        sem_post(&process_sem[reader_index]);
-    */
     return NULL;
 }
+/**
+ * runs the input counter thread responsible for count the occurances of characters put into the input buffer
+ *
+ * @return void*
+ */
 
-void *runInputCounterThread(void *arg)
+void *runInputCounterThread()
 {
-    int read_done = 0;
-    (void)arg;
     int idx = 0;
-    int value;
 
     while (1)
     {
@@ -207,47 +170,25 @@ void *runInputCounterThread(void *arg)
         sem_wait(&inMutex);
         char c = in_buf[idx];
         count_input(c);
-        // printf("in count:%c\n", c);
         sem_post(&inMutex);
         sem_post(&readEmpty);
         idx = (idx + 1) % buffer_size;
-        // printf("EOF?:%d\n", c);
-        // fflush(stdout);
         if (in_buf[idx] == EOF)
         {
             break;
         }
     }
-    /*
-    while (1)
-    {
-        sem_wait(&process_sem[idx]);
-        sem_getvalue(&process_sem[idx], &value);
-        sem_getvalue(&read_finished, &read_done);
-        printf("value:%d encrypt_index:%d reader_index:%d index:%d count:%c\n", value, encrypt_index, reader_index, idx, in_buf[idx]);
-
-        if (idx == reader_index && read_done)
-        {
-            break;
-        }
-        count_input(in_buf[idx]);
-        if (value == 0)
-        {
-            pthread_cond_signal(&buffer_not_full);
-        }
-        idx = ((1 + idx) % buffer_size);
-    }
-    */
     return NULL;
 }
 
-void *runEncryptionThread(void *arg)
+/**
+ * runs the encryption thread responsible for encrypting characters in the input buffer and placing the encrypted characters into the output buffer
+ *
+ * @return void*
+ */
+void *runEncryptionThread()
 {
-    int read_done = 0;
-    int out_value = 0;
-    (void)arg;
     encrypt_index = 0;
-    int value;
     while (1)
     {
         sem_wait(&encryptFull);
@@ -259,59 +200,28 @@ void *runEncryptionThread(void *arg)
         sem_wait(&encryptEmpty);
         sem_wait(&outMutex);
         out_buf[encrypt_index] = c;
-        // printf("encrypt out:%c\n", c);
         sem_post(&outMutex);
         sem_post(&outFull);
         sem_post(&writeFull);
         encrypt_index = (encrypt_index + 1) % buffer_size;
-        // printf("EOF?:%d\n", c);
-        // fflush(stdout);
         if (in_buf[encrypt_index] == EOF)
         {
             out_buf[encrypt_index] = EOF;
             break;
         }
     }
-    /*
-    pthread_mutex_lock(&out_lock);
-    while (1)
-    {
-        sem_wait(&process_sem[encrypt_index]);
-        sem_getvalue(&process_sem[encrypt_index], &value);
-        sem_getvalue(&read_finished, &read_done);
-        if (encrypt_index == reader_index && read_done)
-        {
-            break;
-        }
-        sem_getvalue(&out_process_sem[encrypt_index], &out_value);
-        if (out_value != 0)
-        {
-            pthread_cond_wait(&out_buffer_not_full, &out_lock);
-        }
-        out_buf[encrypt_index] = encrypt(in_buf[encrypt_index]);
-        // printf("encrypt:%c\n", out_buf[encrypt_index]);
-        if (value == 0)
-        {
-            pthread_cond_signal(&buffer_not_full);
-        }
-        sem_post(&out_process_sem[encrypt_index]);
-        sem_post(&out_process_sem[encrypt_index]);
-        encrypt_index = ((1 + encrypt_index) % buffer_size);
-    }
-    pthread_mutex_unlock(&out_lock);
-    sem_post(&encrypt_finished_sem);
-    sem_post(&out_process_sem[encrypt_index]);
-    sem_post(&out_process_sem[encrypt_index]);
-    */
     return NULL;
 }
 
-void *runOutputCounterThread(void *arg)
+/**
+ * output counter thread responsible for counting each occurance of a character in the output buffer
+ *
+ * @return void*
+ */
+
+void *runOutputCounterThread()
 {
-    int encrypt_done = 0;
-    (void)arg;
     int idx = 0;
-    int value;
 
     while (1)
     {
@@ -327,34 +237,18 @@ void *runOutputCounterThread(void *arg)
             break;
         }
     }
-    /*
-    while (1)
-    {
-        sem_wait(&out_process_sem[idx]);
-        sem_getvalue(&encrypt_finished_sem, &encrypt_done);
-
-        if (idx == encrypt_index && encrypt_done)
-        {
-            break;
-        }
-        count_output(out_buf[idx]);
-        sem_getvalue(&out_process_sem[idx], &value);
-        if (value == 0)
-        {
-            pthread_cond_signal(&out_buffer_not_full);
-        }
-        idx = ((1 + idx) % buffer_size);
-    }
-    */
     return NULL;
 }
 
-void *runWriterThread(void *arg)
+/**
+ * writer thread responsible for taking each character in the output buffer and writing it to the output file
+ *
+ * @return void*
+ */
+
+void *runWriterThread()
 {
-    int encrypt_done = 0;
-    (void)arg;
     int idx = 0;
-    int value;
 
     while (1)
     {
@@ -370,26 +264,5 @@ void *runWriterThread(void *arg)
             break;
         }
     }
-    /*
-    while (1)
-    {
-        sem_wait(&out_process_sem[idx]);
-        sem_getvalue(&encrypt_finished_sem, &encrypt_done);
-
-        if (idx == encrypt_index && encrypt_done)
-        {
-            break;
-        }
-        char c = out_buf[idx];
-        // printf("%c\n", c);
-        write_output(c);
-        sem_getvalue(&out_process_sem[idx], &value);
-        if (value == 0)
-        {
-            pthread_cond_signal(&out_buffer_not_full);
-        }
-        idx = ((1 + idx) % buffer_size);
-    }
-    */
     return NULL;
 }
